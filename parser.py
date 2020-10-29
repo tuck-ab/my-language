33f05dc79fb37d30c0ecf6e3a8904ba4c58df380
tuck-ab <alext.5@hotmail.co.uk>
2020-10-29 18:57:08 +0000

    Added a rules file for notes on rules of the language Parser reads source code from file

---
+++ parser.py
@@ -0,0 +1,8 @@
+## -- Read the source code from a file
+file = "test.txt"
+with open(file, "rt") as f:
+    source_code = f.read()
+
+## -- Cleaning the source code
+
+## -- Convert the source code into tokens
