37dfe3d88e4b1d653dbae23f011becd14fc9129a
tuck-ab <alext.5@hotmail.co.uk>
2020-10-30 00:56:15 +0000

    Changed file name to not interfere with python libraries and started the parser

--- parser.py
+++ parser.py
@@ -1,18 +1,32 @@
-import token as t
+import my_token as t
 
-DIGITS = set([str(i) for i in range(10)])
+IGNORE = {" ","\n","\t"}
+DIGITS = {"0","1","2","3","4","5","6","7","8","9"}
+BIN_OPS = {"+","-","*","/"}
 
 class Parser:
     def __init__(self,text):
         self.text = text
         self.pos = -1
         self.current_char = None
-
-        self.tokens = []
+        self.advance()
 
     def advance(self):
         self.pos += 1
         self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
+
+    def generate_tokens(self):
+        tokens = []
+
+        while self.current_char != None:
+            if self.current_char in IGNORE:
+                self.advance()
+            elif self.current_char in DIGITS:
+                self.tokens.append(self.make_number())
+                self.advance()
+            elif self.current_char in BIN_OPS:
+                self.tokens.append(t.Token(t.TT_BINOP,self.current_char))
+                self.advance()
 
     def make_word(self):
         pass
